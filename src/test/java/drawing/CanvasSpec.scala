faa40e37b8f78d4d6f093ff1d56f99b7dbaba7fb
Ayyappan Sampath Ramamuniappa <s.a.ramamuniappa@accenture.com>
2016-09-19 21:11:10 +0100

    Added tests for draw rectangle

--- src/test/java/drawing/CanvasSpec.scala
+++ src/test/java/drawing/CanvasSpec.scala
@@ -1,6 +1,6 @@
 package drawing
 
-import com.springer.nature.drawing.{Canvas, CanvasSize, Line, PointOutsideCanvasException}
+import com.springer.nature.drawing._
 import org.scalatest.FunSpec
 
 /**
@@ -11,28 +11,52 @@ class CanvasSpec extends FunSpec {
   describe("A canvas") {
     describe("when initialised") {
       it("should be empty") {
-        val canvas = new Canvas(CanvasSize(4,4))
+        val canvas = new Canvas(CanvasSize(4, 4))
         assert(canvas.canvas.forall(_.forall(_ == ' ')))
       }
     }
-  }
-  describe("draw line") {
-    it("should draw a horizontal line") {
-      val canvas = new Canvas(CanvasSize(10,4))
-      canvas.drawLine(Line(1,1,3,1))
-      assert(canvas.canvas(0).slice(0,2).forall(_ == 'X'))
-    }
-    it("should draw a vertical line") {
-      val canvas = new Canvas(CanvasSize(10,4))
-      canvas.drawLine(Line(1,1,1,3))
-      for(i <- 0 to 2){
-        assert(canvas.canvas(i)(0) == 'X')
+    describe("draw line") {
+      it("should draw a horizontal line") {
+        val canvas = new Canvas(CanvasSize(10, 4))
+        canvas.drawLine(Line(1, 1, 3, 1))
+        assert(canvas.canvas(0).slice(0, 2).forall(_ == 'X'))
+      }
+      it("should draw a vertical line") {
+        val canvas = new Canvas(CanvasSize(10, 4))
+        canvas.drawLine(Line(1, 1, 1, 3))
+        for (i <- 0 to 2) {
+          assert(canvas.canvas(i)(0) == 'X')
+        }
+      }
+      it("throw PointOutsideCanvasException if point is outside canvas") {
+        assertThrows[PointOutsideCanvasException] {
+          val canvas = new Canvas(CanvasSize(10, 4))
+          canvas.drawLine(Line(1, 1, 1, 5))
+        }
       }
     }
-    it("throw PointOutsideCanvasException if point is outside canvas") {
-      assertThrows[PointOutsideCanvasException]{
-        val canvas = new Canvas(CanvasSize(10,4))
-        canvas.drawLine(Line(1,1,1,5))
+    describe("draw rectangle") {
+      it("should draw a rectangle within the canvas") {
+        val canvas = new Canvas(CanvasSize(10, 10))
+        canvas.drawRectangle(Rectangle(1, 1, 5, 5))
+        //top horizontal line
+        assert(canvas.canvas(0).slice(0, 4).forall(_ == 'X'))
+        //bottom horizontal line
+        assert(canvas.canvas(4).slice(0, 4).forall(_ == 'X'))
+        //left vertical line
+        for (i <- 0 to 2) {
+          assert(canvas.canvas(i)(0) == 'X')
+        }
+        //right vertical line
+        for (i <- 0 to 2) {
+          assert(canvas.canvas(i)(4) == 'X')
+        }
+      }
+      it("throw PointOutsideCanvasException if point is outside canvas") {
+        assertThrows[PointOutsideCanvasException] {
+          val canvas = new Canvas(CanvasSize(10, 10))
+          canvas.drawRectangle(Rectangle(1, 1, 10, 11))
+        }
       }
     }
   }
